e34e91c223f8625ea4be173d7ad22c61104cec63
SmallSpider0 <568442079@qq.com>
2023-10-19 21:39:09 +0800

    优化

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pymongo
 import multiprocessing
 import time
 
-BATCH_SIZE = 10000  # Adjust this as per your needs
+BATCH_SIZE = 1000  # Adjust this as per your needs
 # multiprocessing.cpu_count()
 
 def batched_cursor(cursor, batch_size):
@@ -12,7 +12,7 @@ def batched_cursor(cursor, batch_size):
         batch.append(item)
         if len(batch) == batch_size:
             yield batch
-            batch.clear()
+            batch=[]
     if batch:
         yield batch
 
@@ -20,18 +20,18 @@ def batched_cursor(cursor, batch_size):
 def database_writer(queue, batch_size=100):
     MongoClient_out = pymongo.MongoClient(host="10.12.46.33", port=27018,username="b515",password="sqwUiJGHYQTikv6z")
     collection_out = MongoClient_out['geth']['cnz_output']
-
+    cnt = 0
     batch_data = []  # 用于保存批量数据的列表
     while True:
         data = queue.get()
-
         if data == "STOP":
             # 如果存在未插入的数据，插入它们
+            print(cnt)
             if batch_data:
                 collection_out.insert_many([{'tx_hash': item['tx_hash'],'call':item['call']} for item in batch_data])
                 batch_data.clear()
             break
-
+        cnt+=1
         batch_data.append(data)
 
         # 当达到批量大小时进行写入
@@ -39,46 +39,43 @@ def database_writer(queue, batch_size=100):
             collection_out.insert_many([{'tx_hash': item['tx_hash'],'call':item['call']} for item in batch_data])
             batch_data.clear()  # 清空列表以准备下一批数据
 
-def wrapper_func(args):
-    return process_record(*args)
 
-def main():
+if __name__ == "__main__":
     # 远程连接服务器中mongodb，选中transaction集合
     MongoClient = pymongo.MongoClient(host="10.12.46.33", port=27018,username="b515",password="sqwUiJGHYQTikv6z")
     collection = MongoClient['geth']['transaction']
 
     # 查询规则
     query = {
-        "tx_blocknum": {"$gt": 4000000, "$lt": 4100000},
+        "tx_blocknum": {"$gt": 4000000, "$lt": 4010000},
         "tx_trace": {"$ne": ""}
     }
     cursor = collection.find(query).batch_size(1000)
-    BC = batched_cursor(cursor,BATCH_SIZE)
 
+    BC = batched_cursor(cursor,BATCH_SIZE)
     # 初始化消息队列
     queue = multiprocessing.Manager().Queue(1000)
-
+    
     # 创建单独的数据库写入进程
     db_writer_process = multiprocessing.Process(target=database_writer, args=(queue,))
     db_writer_process.start()
 
     st = time.time()
-
-    with multiprocessing.Pool(8) as pool:
+    cnt = 0
+    with multiprocessing.Pool(4) as pool:
         # 处理每个batch
         while True:
             try:
                 # 处理一个batch
                 batch = next(BC) #需要一定时间
-                pool.map(wrapper_func, [(item, queue) for item in batch])
+                cnt += len(batch)
+                pool.apply_async(process_record, (batch, queue,))
             except StopIteration:
-                # 结束后关闭队列
+                print(cnt)
+                pool.close()  # 关闭进程池，不再接受新的任务
+                pool.join()   # 等待所有任务完成
                 queue.put("STOP")
                 break
 
-    queue.put("STOP")
     db_writer_process.join()
     print(time.time()-st)
-
-if __name__ == "__main__":
-    main()
