c4d45871fc5246df554930c05e2644ff7cad6dfc
SmallSpider0 <568442079@qq.com>
2023-10-20 10:59:20 +0800

    优化性能

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ import json
 
 BATCH_SIZE = 1000
 
-# 调整该参数 使瓶颈在远端的mongodb上
+# 调整以下参数 使瓶颈在远端的mongodb上
 # 若提高某个参数能提升CPU占用率，说明瓶颈还在本地
 #
 #处理数据的进程数量
 process_cnt = 1
+#处理返回值的进程数量
+output_handle_cnt = 1
+# 写入数据库的进程数量默认为1，因为瓶颈在数据库的写入速度上
 
 def batched_cursor(cursor, batch_size):
     batch = []
@@ -24,12 +27,16 @@ def batched_cursor(cursor, batch_size):
     if batch:
         yield batch
 
-def process_output_handle(queue_in, queue_out):
+def process_output_handle(queue_in, queue_out, counter):
     # handle_BSONObjectTooLarge
     while True:
         item = queue_in.get()
         if item == "STOP":
-            queue_out.put("STOP")
+            # Decrease the counter when one process has finished its tasks
+            with counter.get_lock():
+                counter.value -= 1
+                if counter.value == 0:
+                    queue_out.put("STOP")
             break
         if sys.getsizeof(json.dumps(item)) > 15 * 1024 * 1024:  # 10MB as a buffer
             print(f"Warning: Large document of size {sys.getsizeof(json.dumps(item))} bytes")
@@ -68,25 +75,29 @@ if __name__ == "__main__":
 
     # 查询规则
     query = {
-        "tx_blocknum": {"$gt": 4000000, "$lt": 4100000},
+        "tx_blocknum": {"$gt": 4000000, "$lt": 4010000},
         "tx_trace": {"$ne": ""}
     }
     cursor = collection.find(query).batch_size(500)
-
     BC = batched_cursor(cursor,BATCH_SIZE)
+
     # 初始化消息队列
     queue_process = multiprocessing.Manager().Queue(500)
     queue_db = multiprocessing.Manager().Queue(500)
     
     # 创建独立的返回数据处理进程
-    output_handle = multiprocessing.Process(target=process_output_handle, args=(queue_process, queue_db,))
-    output_handle.start()
-
+    counter = multiprocessing.Value('i', output_handle_cnt)
+    output_handle_processes = []
+    for _ in range(output_handle_cnt):
+        p = multiprocessing.Process(target=process_output_handle, args=(queue_process, queue_db, counter))
+        p.start()
+        output_handle_processes.append(p)
+    
     # 创建独立的数据库写入进程
     db_writer_process = multiprocessing.Process(target=process_database_writer, args=(queue_db,))
     db_writer_process.start()
 
-
+    # 主进程
     st = time.time()
     cnt = 0
     max_tasks = process_cnt * 10
@@ -108,7 +119,9 @@ if __name__ == "__main__":
                 print(cnt)
                 pool.close()  # 关闭进程池，不再接受新的任务
                 pool.join()   # 等待所有任务完成
-                queue_process.put("STOP")
+                # 结束所有返回数据处理进程
+                for _ in range(output_handle_cnt):
+                    queue_process.put("STOP")
                 break
 
     # 等待其它进程完成
